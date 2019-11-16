b003589cef6479a260a3f7b4989e179d432a46f7
aelvismorales <faviotio123@gmail.com>
2019-11-16 16:28:53 -0500

    Agregacion Lista que contiene los valores de los archivos

--- SW32-TF-Algoritmos/Source.cpp
+++ SW32-TF-Algoritmos/Source.cpp
@@ -7,88 +7,119 @@
 #include<Windows.h>
 #include<vector>
 #include <string>
+#include<functional>
 using namespace std;
 using namespace std::experimental::filesystem;
 namespace fs = std::experimental::filesystem;
 
 using namespace std::chrono_literals;
-void imprimir(int e) {
-	cout << " " << e;
-}
-void imprime(string p) 
+void imprimir(int e)
 {
-	cout << " " << p;
+	cout << " , " << e;
 }
+void imprime(string p)
+{
+	cout << " , " << p;
+}
+class Libreta {
+	string name;
+	int size;
+	string extension; // good reads
+	string timeupdate;
+public:
+	Libreta(string name, int size, string exte, string timeupdate)
+		: name(name), size(size), extension(exte), timeupdate(timeupdate) {}
+
+	string getName() { return name; }
+	int getSize() { return size; }
+	string getExtension() { return extension; }
+	string getTimeUpdate() { return timeupdate; }
+	//void setname(string name) { this->name = name; }
+	//void setyear(int year) { this->size = year; }
+	//void setscore(string score) { this->extension = score; }
+	//void setTimeupdate(string update) { this->timeupdate = update; }
+
+};
 ///------
 int main() {
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 4996)
 #endif
-	ArbolAVL<int>* tree;
 	ArbolAVL<int>* sizefile;
+	ArbolAVL<string>*  extension;
+	ArbolAVL<string>* filename;
+	ArbolAVL<string>* timeupdate;
+
 	sizefile = new ArbolAVL<int>(imprimir);
-	tree = new ArbolAVL<int>(imprimir);
-	ArbolAVL<string>* arbolito;
-	ArbolAVL<string>* file;
-	ArbolAVL<string>* fecha;
-	file = new ArbolAVL<string>(imprime);
-	arbolito = new ArbolAVL<string>(imprime);
-	fecha = new ArbolAVL<string>(imprime);
-	auto ftime = last_write_time("C:\\Users\\Aldo\\Pictures\\Saved Pictures");
+	filename = new ArbolAVL<string>(imprime);
+	extension = new ArbolAVL<string>(imprime);
+	timeupdate = new ArbolAVL<string>(imprime);
+
+
+
+
+
+	uintmax_t valorpeso;
+	auto ftime = last_write_time("C:\\Users\\USER\\Desktop\\FISICA LABORATORIO");
 
 	std::time_t cftime = decltype(ftime)::clock::to_time_t(ftime); // assuming system_clock
 
-	string path1 = "C:\\Users\\Aldo\\Pictures\\Saved Pictures";  // . es la careta del programa // investigar c++files system
-	string path2 = "C:\\Users\\Aldo\\Pictures\\Saved Pictures";
-	string path3 = "C:\\Users\\Aldo\\Pictures\\Saved Pictures";
-	string path4 = "C:\\Users\\Aldo\\Pictures\\Saved Pictures";
+	string path1 = "C:\\Users\\USER\\Desktop\\FISICA LABORATORIO";  // . es la careta del programa // investigar c++files system
+	string path2 = "C:\\Users\\USER\\Desktop\\FISICA LABORATORIO";
+	string path3 = "C:\\Users\\USER\\Desktop\\FISICA LABORATORIO";
 
 	for (const auto & entry : directory_iterator(path1)) {
-		cout << entry.path() << endl;
-		cout << path(entry.path()).filename() << endl;
-		/*path2 = path(entry.path()).filename();*/
-		cout << file_size(entry.path()) << endl;
-		cout << path(entry.path()).extension() << endl;
-		ftime = last_write_time(entry.path());
-		cftime = decltype(ftime)::clock::to_time_t(ftime);
-		path2 = path(entry.path().filename()).u8string();
-		path3= path(entry.path().extension()).u8string();
-		path4 = asctime(localtime(&cftime));
-		cout << "File write time is " << asctime(localtime(&cftime));
-		sizefile->insertar(file_size(entry.path()));
-		file->insertar(path2);
-		arbolito->insertar(path3);
-		fecha->insertar(path4);
+		/*	cout << entry.path() << endl;
 
+		cout << path(entry.path()).filename() << endl;*/
+
+		/*	path2 = path(entry.path()).filename();
+
+		cout << file_size(entry.path()) << endl;
+
+		cout << path(entry.path()).extension() << endl;*/
+
+		ftime = last_write_time(entry.path());
+
+		cftime = decltype(ftime)::clock::to_time_t(ftime);
+
+		path2 = path(entry.path().filename()).u8string();
+
+		path3 = path(entry.path().extension()).u8string();
+
+		valorpeso = file_size(entry.path());
+
+		Libreta* libreta = new Libreta(path2, valorpeso, path3, asctime(localtime(&cftime)));
+
+		sizefile->insertar(libreta->getSize());
+
+		filename->insertar(libreta->getName());
+
+		extension->insertar(libreta->getExtension());
+
+		timeupdate->insertar(libreta->getTimeUpdate());
 	}
 
-
-	srand(time_t(NULL));
-	//--
-
-	//--
-	
-	//----
-	/*cout << "Randon Number Generating..." << endl;
-	for (int i = 0; i < 7; i++) {
-		int val = 1 + rand() % 100;
-		cout << "Inserting(";
-		imprimir(val);
-		cout << ")" << endl;
-		///----
-		tree->insertar(val);
-	}*/
-	//--
-	tree->inOrder();
-	cout << "\n";
+	cout << "Peso de Archivos:" << endl;
+	cout << "------------------" << endl;
 	sizefile->inOrder();
 	cout << "\n";
-	file->inOrder();
+	cout << "---------------------";
 	cout << "\n";
-	arbolito->inOrder();
+	cout << "Nombre archivos:" << endl;
+	cout << "-----------------------" << endl;
+	filename->inOrder();
 	cout << "\n";
-	fecha->inOrder();
+	cout << "------------------------";
+	cout << "\n";
+	cout << "Extension:" << endl;
+	cout << "--------------------" << endl;
+	extension->inOrder();
+	cout << "\n";
+	cout << "Fecha Time Update:" << endl;
+	cout << "--------------------" << endl;
+	timeupdate->inOrder();
 	cout << "\n";
 	system("pause");
 #ifdef _MSC_VER
